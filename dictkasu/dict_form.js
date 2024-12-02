68b988bc614a033be1538148305eb4cbbd42249c
oikasu1 <weisngkasu01@gmail.com>
2024-12-02 20:22:51 +0800

    0.0

--- dictkasu/dict_form.js
+++ dictkasu/dict_form.js
@@ -1,16 +1,24 @@
-var varForm="";
-
-varForm +=" <div class=\"search\"> "; 
-varForm +=" <div class=\"autocomplete\"> "; 
-varForm +=" <button id=\"myWordOX\" onclick=\"myWordOX()\">⊕</button> "; 
-varForm +=" <input type=\"text\" class=\"mySearch\" value=\"\" id=\"AAA\" placeholder=\"" + btnT + "\" autofocus onkeypress=\"if(event.keyCode == 13){Kasu('K'); myWordOX2();}\"> "; 
-varForm +=" <span class=\"btn-group\"> "; 
-varForm +=" <button onclick=\"Kasu('K'); myWordOX2();\">" + btnK + "</button><button onclick=\"Kasu('G'); myWordOX2();\">" + btnG + "</button> "; 
-varForm +=" </span>   "; 
-varForm +=" </div> "; 
-varForm +=" <script>myWord();</script> "; 
-varForm +=" <br /> "; 
-varForm +=" <div id=\"BBB\"></div> "; 
-varForm +=" </div> ";
+const varForm = `
+  <h3>🥷烏衣行 詔安客語小辭典</h3>
+  <div class="search">
+    <div class="autocomplete">
+      <button id="myWordOX" onclick="myWordOX()">⊕</button>
+      <input 
+        type="text" 
+        class="mySearch" 
+        id="AAA" 
+        placeholder="${btnT}" 
+        autofocus 
+        onkeypress="if(event.keyCode == 13){Kasu('K'); myWordOX2();}">
+		<span class="btn-group">
+		  <button id="btnK" onclick="Kasu('K'); myWordOX2();">${btnK}</button>
+		  <button id="btnG" onclick="Kasu('G'); myWordOX2();">${btnG}</button>
+		</span>
+    </div>
+    <script>myWord();</script>
+    <br />
+    <div id="BBB"></div>
+  </div>
+`;
 
 document.write(varForm);
